7b4f396b35be49d0e273001f64941b84ed76ee48
ranranqi <1137625794@qq.com>
2018-10-22 20:22:18 +0800

    模块化避免全局变量干扰使用 !function(){}.call()

--- js/init-swiper.js
+++ js/init-swiper.js
@@ -1,10 +1,12 @@
-var mySwiper = new Swiper('.swiper-container', {
-    loop: true,
-    pagination: {
-        el: '.swiper-pagination',
-    },
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-})
+!function(){
+    var mySwiper = new Swiper('.swiper-container', {
+        loop: true,
+        pagination: {
+            el: '.swiper-pagination',
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    })
+}.call()
--- js/sticky-topbar.js
+++ js/sticky-topbar.js
@@ -1,7 +1,10 @@
-window.addEventListener('scroll', function (xxx){
-    if (window.scrollY > 0) {
-        topNavBar.classList.add('sticky')
-    } else {
-        topNavBar.classList.remove('sticky')
-    }
-})
+!function(){
+    window.addEventListener('scroll', function (xxx){
+        if (window.scrollY > 0) {
+            topNavBar.classList.add('sticky')
+        } else {
+            topNavBar.classList.remove('sticky')
+        }
+    })
+}.call()
+
