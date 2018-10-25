4cba3f45e6976edad1caa820eec879ac8b6a4a0e
ranranqi <1137625794@qq.com>
2018-10-25 17:11:47 +0800

    添加CSS，无刷新留言

--- js/message.js
+++ js/message.js
@@ -13,7 +13,7 @@ query.find()
             let array = messages.map((item)=> item.attributes)
             array.forEach((item)=>{
                 let li = document.createElement('li')
-                li.innerText = item.content
+                li.innerText = `${item.name}: ${item.content}`
                 let messageList = document.querySelector('#messageList')
                 messageList.append(li)
             })
@@ -25,12 +25,18 @@ let myForm = document.querySelector('#postMessageForm')
 myForm.addEventListener('submit',function(e){
     e.preventDefault()
     let content = myForm.querySelector('input[name=content]').value
+    let name = myForm.querySelector('input[name=name]').value
     var Message = AV.Object.extend('Message');
     var message = new Message();
     message.save({
+        'name': name,
         'content': content
       }).then(function(object) {
-        window.location.reload()  //成功之后自动刷新页面
+        let li = document.createElement('li')
+        li.innerText = `${object.attributes.name}: ${object.attributes.content}`
+        let messageList = document.querySelector('#messageList')
+        messageList.append(li)
+        myForm.querySelector('input[name=content]').value = ''
       })
 })
 
