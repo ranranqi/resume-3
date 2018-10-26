92fa75df1a91f660d00a9824ea18c96adb1e5e41
ranranqi <1137625794@qq.com>
2018-10-26 14:16:05 +0800

    生成一个 View,Model模板

--- js/message.js
+++ js/message.js
@@ -1,30 +1,7 @@
 ! function () {
-    var model = {
-        // 获取数据
-        init: function () {
-            var APP_ID = '5Okc1YErrmYfWMlQBDaOW0PJ-gzGzoHsz';
-            var APP_KEY = 'sNBrN4Tkn5XyQJSy2ThDrUG1';
-            AV.init({
-                appId: APP_ID,
-                appKey: APP_KEY
-            })
-        },
-        fetch: function () {
-            var query = new AV.Query('Message');
-            return query.find() // Promise 对象
-        },
-        // 创建数据
-        save: function (name, content) {
-            var Message = AV.Object.extend('Message');
-            var message = new Message();
-            return message.save({ // Promise 对象
-                'name': name,
-                'content': content
-            })
-        }
-    }
+    var model = Model({resourceName:'Message'})
 
-    var view = document.querySelector('section.message')
+    var view = View('section.message')
 
 
     var controller = {
@@ -54,7 +31,7 @@
             )
         },
         bindEvents: function () {
-            this.form.addEventListener('submit', function (e) {
+            this.form.addEventListener('submit', (e)=> {
                 e.preventDefault()
                 this.saveMessage()
             })
@@ -63,7 +40,9 @@
             let myForm = this.form
             let content = myForm.querySelector('input[name=content]').value
             let name = myForm.querySelector('input[name=name]').value
-            this.model.save(name, content).then(function (object) {
+            this.model.save({
+                'name': name, 'content': content
+            }).then(function (object) {
                 let li = document.createElement('li')
                 li.innerText = `${object.attributes.name}: ${object.attributes.content}`
                 let messageList = document.querySelector('#messageList')
